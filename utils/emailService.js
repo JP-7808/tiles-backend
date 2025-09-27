d7e99d5932135648bc1334a39d3f177328d4d898
JP-7808 <109643541+JP-7808@users.noreply.github.com>
2025-09-28 02:28:41 +0530

    Update emailService.js

--- utils/emailService.js
+++ utils/emailService.js
@@ -1,29 +1,26 @@
-// emailService.js
 import nodemailer from 'nodemailer';
 import dotenv from 'dotenv';
+
 dotenv.config();
 
-const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST,
-  port: process.env.EMAIL_PORT,
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
-  }
-});
-
-// define the function
 const sendEmail = async (options) => {
+  const transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
   const mailOptions = {
     from: process.env.EMAIL_FROM,
     to: options.email,
     subject: options.subject,
     text: options.message,
-    html: options.html
   };
 
   await transporter.sendMail(mailOptions);
 };
 
-// export it as default
 export default sendEmail;

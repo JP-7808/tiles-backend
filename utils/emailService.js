6c61ab867f6845e84baffb7cea222a207d26dbe2
JP-7808 <109643541+JP-7808@users.noreply.github.com>
2025-09-28 01:18:05 +0530

    Update emailService.js

--- utils/emailService.js
+++ utils/emailService.js
@@ -4,17 +4,19 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const sendEmail = async (options) => {
+  // Use port 465 for a secure SSL connection
   const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
+    port: process.env.EMAIL_PORT || 465, // Default to 465
+    secure: true, // Use SSL
     auth: {
       user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
+      pass: process.env.EMAIL_PASS, // This MUST be your App Password
     },
   });
 
   const mailOptions = {
-    from: process.env.EMAIL_FROM,
+    from: `"TileCraft" <${process.env.EMAIL_FROM}>`, // A better "from" format
     to: options.email,
     subject: options.subject,
     text: options.message,
